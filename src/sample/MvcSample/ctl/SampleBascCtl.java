99f0787054a58dd71cb982146bed592ae112e0c8
Young <tmznt11@naver.com>
2024-08-27 17:28:17 +0900

    방가방가

--- src/sample/MvcSample/ctl/SampleBascCtl.java
+++ src/sample/MvcSample/ctl/SampleBascCtl.java
@@ -66,6 +66,7 @@ public class SampleBascCtl {
 		log.info("sampleBascSelect START");
 		log.info("sampleBascSelect START");
 		log.info("sampleBascSelect START");
+		log.info("sampleBascSelect START");
         
 		String contextPath = request.getContextPath();
 
